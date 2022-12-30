e9fa3bf26858ebe929ce6942bd32babc8c3b47b3
Lake <lake@smailnet.com>
2022-12-30 21:38:54 +0800

    Update sample code

--- src/test/java/sample/Department.java
+++ src/test/java/sample/Department.java
@@ -6,12 +6,10 @@ import java.util.List;
 import java.util.function.Consumer;
 
 final class Department extends QuickIO.Object {
-
     String name;
     List<Long> studentIds;
 
     Department(Consumer<Department> consumer) {
         consumer.accept(this);
     }
-
 }
--- src/test/java/sample/Student.java
+++ src/test/java/sample/Student.java
@@ -5,7 +5,6 @@ import com.github.artbits.quickio.QuickIO;
 import java.util.function.Consumer;
 
 final class Student extends QuickIO.Object {
-
     String name;
     Integer gender;
     Integer age;
@@ -20,11 +19,4 @@ final class Student extends QuickIO.Object {
     Student(Consumer<Student> consumer) {
         consumer.accept(this);
     }
-
-    @Override
-    public String toString() {
-        String s = gender == Gender.MALE ? "Male" : "Female";
-        return String.format("%d %s %s %d", this.id(), name, s, age);
-    }
-
 }
