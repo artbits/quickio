71b53e80797387cb01a73ef2e228680b77637eee
artbits <zguanhu@foxmail.com>
2022-12-30 21:36:29 +0800

    Add JSONObject class Optimize FindOptions class Optimize other code

--- src/main/java/com/github/artbits/quickio/FindOptions.java
+++ src/main/java/com/github/artbits/quickio/FindOptions.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Zhang Guanhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.artbits.quickio;
 
 import java.lang.reflect.Field;
@@ -22,14 +38,10 @@ public final class FindOptions<T> {
 
 
     public FindOptions sort(String fieldName, int value) {
-        if (fieldName == null || fieldName.isEmpty()) {
-            throw new RuntimeException("The first parameter of sort method cannot be null or empty");
-        }
-        if (value < -1 || value > 1) {
-            throw new RuntimeException("The second parameter of the sort method can only be 1 or -1");
-        }
-        sortFieldName = fieldName;
-        sortValue = value;
+        sortFieldName = Optional.ofNullable((fieldName == null || fieldName.isEmpty()) ? null : fieldName)
+                .orElseThrow(() -> new RuntimeException("The first parameter cannot be null or empty"));
+        sortValue = Optional.ofNullable((value < -1 || value > 1) ? null : value)
+                .orElseThrow(() -> new RuntimeException("The second parameter can only be 1 or -1"));
         return this;
     }
 
